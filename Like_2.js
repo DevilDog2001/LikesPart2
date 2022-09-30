ae016746ce26c7a8f5cc5a968efcf6f57f92674d
Richardson Antoine <91235784+DevilDog2001@users.noreply.github.com>
2022-09-29 23:00:16 -0400

    Add files via upload

---
+++ Like_2.js
@@ -0,0 +1,26 @@
+var y = 1
+
+var v = 1
+
+var z = 1 
+
+var a = document.getElementById("liked")
+
+var b = document.getElementById("liker")
+
+var x = document.getElementById("likes")
+
+function count(){
+    y++;
+    x.innerText = 8+y+" like(s)";
+}
+
+function count1(){
+    v++;
+    a.innerText = 11+v+" like(s)";
+}
+
+function count2(){
+    z++;
+    b.innerText = 8+z+" like(s)";
+}
